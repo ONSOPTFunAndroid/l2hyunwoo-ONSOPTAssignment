d24c1cb663323b2860630bedf5291f6cf7048e3c
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-10-22 22:13:20 +0900

    BaseRecyclerView Compelte

--- app/src/main/java/com/example/onsoptfirstassignment/detail/viewmodel/WelcomeViewModel.kt
+++ app/src/main/java/com/example/onsoptfirstassignment/detail/viewmodel/WelcomeViewModel.kt
@@ -1,15 +1,24 @@
 package com.example.onsoptfirstassignment.detail.viewmodel
 
 import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.onsoptfirstassignment.R
+import com.example.onsoptfirstassignment.detail.model.ProjectData
+import com.example.onsoptfirstassignment.detail.model.initProjectList
 import com.example.onsoptfirstassignment.preference.LoginPreference
 
 class WelcomeViewModel : ViewModel() {
 
     val welcomeText = ObservableField<String>()
+    val _projectList = MutableLiveData<List<ProjectData>>()
+    val projectList : LiveData<List<ProjectData>>
+        get() = _projectList
 
     init {
         setText()
+        _projectList.postValue(initProjectList())
     }
 
     private fun setText() {
